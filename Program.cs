6be1b95b50f87e2300830aa6e87a1f555dcac829
Kelar Crisp <kelcrisp007@icloud.com>
2019-08-17 14:44:38 -0400

    just some playijng around with different linq methods

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace newPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a setence to be reversed");
+            string userAnswer = Console.ReadLine();
+
+            string[] wordArray = userAnswer.Split(' ');
+            foreach (string w in wordArray.Reverse())
+                Console.Write(w + " ");
+
+            Console.ReadLine();
+
+
+        }
+
+
+    }
+}
